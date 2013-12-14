6517f0d596a3b03a105caefa65eb15d307fbb6b0
julien genoud <comicke@hotmail.fr>
2013-12-14 18:20:12 +0100

    refractoriastion + add basic menu + add gui contoller

--- bobby.js
+++ bobby.js
@@ -1,12 +1,11 @@
-
 var camera, scene, renderer;
 var mesh;
-var caube;
 var mouseX = 0, mouseY = 10;
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
 var rotate;
 var hero_obj;
+var plane;
 
 		// var audio = document.getElementById("music")
 		// audio.volume=0.2;
@@ -21,8 +20,6 @@ function init() {
 	document.body.appendChild( renderer.domElement );
 
 	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
-	camera.position.z = 400;
-
 	scene = new THREE.Scene();
 
 	var geometry = new THREE.PlaneGeometry(121,121, 121, 121);
@@ -32,65 +29,56 @@ function init() {
 	plane.position.y -= 0.5;
 	plane.rotation.x -= 1.571;
 
-	var geometry = new THREE.SphereGeometry(0.6,32,16);
-	var material = new THREE.MeshLambertMaterial( { color: 0xffffff } );
-	hero = new THREE.Mesh( geometry, material );
-	scene.add( hero );
+	var Gtitle = new THREE.TextGeometry( "{Bobby Carrot}", {
+		size: 3, height: 0.5, curveSegments: 3,
+		font: "helvetiker", weight: "bold", style: "normal",
+	});
 
-	var helper = new THREE.WireframeHelper( hero );
-	helper.material.depthTest = true;
-	helper.material.opacity = 1;
-	helper.material.transparent = true;	
-	helper.material.color.set( 0x0000ff );
-	scene.add( helper );
-
-
-
-	var geometry_volume = new THREE.TextGeometry( "VOLUME", {
-		size: 1.2, height: 0.2, curveSegments: 3,
+	var plus = new THREE.TextGeometry( "Create  •", {
+		size: 1.1, height: 0.2, curveSegments: 3,
 		font: "helvetiker", weight: "normal", style: "normal",
 	});
 
-	var plus = new THREE.TextGeometry( "+", {
-		size: 1.3, height: 0.2, curveSegments: 3,
+	var moins = new THREE.TextGeometry( "Play      •", {
+		size: 1.1, height: 0.2, curveSegments: 3,
 		font: "helvetiker", weight: "normal", style: "normal",
 	});
 
-	var moins = new THREE.TextGeometry( "-", {
-		size: 1.3, height: 0.2, curveSegments: 3,
+	var Gmenu = new THREE.TextGeometry( "--------------------------", {
+		size: 1.2, height: 0.4, curveSegments: 3,
 		font: "helvetiker", weight: "normal", style: "normal",
 	});
 
-	var zero = new THREE.TextGeometry( "stop", {
-		size: 0.5, height: 0.15, curveSegments: 3,
-		font: "helvetiker", weight: "normal", style: "normal",
-	});
+	var Material_characters = new THREE.MeshLambertMaterial( { color: 0xEEEEEE} );
 
-	var red1 = new THREE.MeshLambertMaterial( { color: 0xEEEEEE} );
-	var red2 = new THREE.MeshLambertMaterial( { color: 0xEEEEEE} );
-	var play = new THREE.MeshLambertMaterial( { color: 0xEEEEEE} );
-	minus = new THREE.Mesh( moins, red1 );
-	maxus = new THREE.Mesh( plus, red2 );
-	play = new THREE.Mesh( geometry_volume, play );
-	zerom = new THREE.Mesh( zero, red2 );
+	mesh_title = new THREE.Mesh( Gtitle, Material_characters );
+	mesh_menu = new THREE.Mesh( Gmenu, Material_characters );
+	minus = new THREE.Mesh( moins, Material_characters );
+	maxus = new THREE.Mesh( plus, Material_characters );
+
 
 	scene.add( minus );
 	scene.add( maxus );		
-	scene.add( play );
-	scene.add( zerom );
+	scene.add( mesh_title );
+	scene.add( mesh_menu );
 
 	minus.rotation.x  -= 1.571;
 	maxus.rotation.x  -= 1.571;
-	zerom.rotation.x  -= 1.571;
-	play.rotation.x  -= 1;
+	mesh_menu.rotation.x  -= 1;
+	mesh_title.rotation.x  -= 0.8;
 
 	minus.rotation.y  += 0.15;
 	maxus.rotation.y  += 0.15;
-	zerom.rotation.y  += 0.15;
+	mesh_menu.rotation.y  += 0;
 
 	camera.position.z = 5;
 	camera.position.y = 10;
 	camera.rotation.x -= 1;
+
+	setbasepostion (-7.8,0.5,minus);
+	setbasepostion (-7.8,2.5,maxus);
+	setbasepostion (-8,-2,mesh_menu);
+	setbasepostion (-15,-7,mesh_title);
 
 
 	//red
@@ -106,17 +94,21 @@ function init() {
 	light3.position.set( 2, 2, 3 );
 	scene.add( light3 );
 	//violet
-	var lightw = new THREE.PointLight( 0x9922ff, 0.6, 5 );
-	lightw.position.set( -2, 2, 2 );
-	scene.add( lightw );
-	//?
-	var lightw = new THREE.PointLight( 0x0000ff, 0.8, 30 );
-	lightw.position.set( 8, 2, -6 );
-	scene.add( lightw );
-	//blue light
-	var lightw = new THREE.PointLight( 0xff4444, 0.8, 20 );
-	lightw.position.set( -9, 2, 0 );
-	scene.add( lightw );
+	var light4 = new THREE.PointLight( 0x9922ff, 0.6, 5 );
+	light4.position.set( -2, 2, 2 );
+	scene.add( light4 );
+	//blue light (ond a droite)
+	var light5 = new THREE.PointLight( 0x0000ff, 0.8, 30 );
+	light5.position.set( 8, 2, -6 );
+	scene.add( light5 );
+	//red light
+	var light6 = new THREE.PointLight( 0xff4444, 0.8, 20 );
+	light6.position.set( -9, 2, 0 );
+	scene.add( light6 );
+	//white
+	var light7 = new THREE.PointLight( 0x00ffff, 0.6, 10 );
+	light7.position.set( 0, 0, -8);
+	scene.add( light7);
 	//?
 	var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.09 );
 	directionalLight.position.set( 0, 2, 0 );
@@ -131,15 +123,39 @@ function init() {
 	stats.domElement.style.top = '0px';
 	container.appendChild( stats.domElement );
 
-	setbasepostion (-7.8,0.7,minus);
-	setbasepostion (-4.4,0.7,maxus);
-	setbasepostion (-6.4,0.4,zerom);
-	setbasepostion (-9,-1,play);
+
 
 	var jsonLoader = new THREE.JSONLoader();
 	jsonLoader.load( "objs/bunny_obj.js", addModelToScene ); 
-	setbasepostion (0,2,hero);
+
 }
+
+window.onload = function() {
+  var gui = new dat.GUI();
+
+	var f1 = gui.addFolder('hero position');
+	f1.add(hero_obj.position, 'x', -5, 5);
+	f1.add(hero_obj.position, 'y', -5, 5);
+	f1.add(hero_obj.position, 'z', -5, 5);
+
+
+	var f2 = gui.addFolder('hero size');
+	f2.add(hero_obj.scale, 'x', -5, 5);
+	f2.add(hero_obj.scale, 'y', -5, 5);
+	f2.add(hero_obj.scale, 'z', -5, 5);
+
+	var f3 = gui.addFolder('plane');
+	f3.add(plane.rotation, 'x', -5, 5);
+	f3.add(plane.rotation, 'y', -5, 5);
+	f3.add(plane.rotation, 'z', -5, 5);
+
+	// var f4 = gui.addFolder('plane2');
+	// f4.add(plane.rotation, 'x', -5, 5);
+	// f4.add(plane.rotation, 'y', -5, 5);
+	// f4.add(plane.rotation, 'z', -5, 5);
+
+	gui.close();
+};
 
 
 function addModelToScene( geometry, materials ) {
@@ -147,9 +163,10 @@ function addModelToScene( geometry, materials ) {
 	hero_obj = new THREE.Mesh( geometry, material );
 	hero_obj.scale.set(0.18,0.18,0.18);
 	scene.add( hero_obj );
-	setbasepostion (2,2,hero_obj);
+	setbasepostion (2,1,hero_obj);
 	hero_obj.position.y = 0.4;
 };
+
 
 function setbasepostion ( a , b, elem ) {
 
@@ -169,31 +186,27 @@ function onKeyDown ( event ) {
 	//39 droite
 
 	case 37: /*gauche*/
-	console.log("pos en x :" + hero.position.x);
-		//hero_obj.position.x -= 1;
-		//hero_obj.rotation.y = 0.1;
-		hero.position.x -= 1;
+	console.log("pos en x :" + hero_obj.position.x);
+		hero_obj.position.x -= 1;
+		hero_obj.rotation.y = 0.1;
 		break;
 
 		case 39: /*droite*/
-		console.log("pos en x :" + hero.position.x);
-		//					hero_obj.rotation.y = 3.5;
-		//					hero_obj.position.x += 1;
-		hero.position.x += 1;
+		console.log("pos en x :" + hero_obj.position.x);
+		hero_obj.rotation.y = 3.5;
+		hero_obj.position.x += 1;
 		break;
 
 		case 38: /*haut*/
-		console.log("pos en z :" + -hero.position.z);
-		//					hero_obj.rotation.y = 5;
-		//					hero_obj.position.z -= 1;
-		hero.position.z -= 1;
+		console.log("pos en z :" + -hero_obj.position.z);
+		hero_obj.rotation.y = 5;
+		hero_obj.position.z -= 1;
 		break;
 
 		case 40: /*bas*/
-		console.log("pos en z :" + -hero.position.z);
-		//					hero_obj.rotation.y = 1;
-		//					hero_obj.position.z += 1;
-		hero.position.z += 1;
+		console.log("pos en z :" + -hero_obj.position.z);
+		hero_obj.rotation.y = 1;
+		hero_obj.position.z += 1;
 		break;	
 	}
 };
