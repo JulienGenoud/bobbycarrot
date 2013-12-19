c7b393e8fade15dedbd131267b6fabbbef659de4
Julien Genoud <juliengenoud@gmail.com>
2013-12-20 00:19:45 +0100

    restore server.sh

--- serve.sh
+++ serve.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Starts a basic web server on the port specified.
-# 
+#
 # ./serve.sh 3000 -> http://localhost:3000
 #
 # Copyright 2012 Eric Bidelman <ebidel@gmail.com>
@@ -20,3 +20,4 @@ else
 fi
 
 $open http://localhost:$port/index.html && python -m SimpleHTTPServer $port;
+
